86ce4e333e78abf9434319c4de6270ad1d6c34e6
Mani Movassagh <mani.mgh@gmail.com>
2021-11-30 22:33:21 +0100

    Rust ReadLine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use proc_macro::bridge::PanicMessage::String;
+use std::io;
+
 fn main() {
     println!("Hi Mani Welcome to Learn Rust in Intellij , It is really perfect ");
+    let mut guess = String::new();
+    io::stdin().read_line(buf);
 }
+
