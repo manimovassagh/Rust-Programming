b01eef1b80d3d0e1c60d121f16698539b515fe9f
Mani Movassagh <mani.mgh@gmail.com>
2021-11-30 22:36:57 +0100

    Rust Linein

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use proc_macro::bridge::PanicMessage::String;
+
 use std::io;
 
 fn main() {
     println!("Hi Mani Welcome to Learn Rust in Intellij , It is really perfect ");
     let mut guess = String::new();
-    io::stdin().read_line(buf);
+    io::stdin().read_line(&mut guess).expect("Failed to read Line");
+    println!("You guessed:{}",guess)
 }
 
