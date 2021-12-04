fd46d4e1f41a800069c619f97738ac09f02adc7c
Mani Movassagh <mani.mgh@gmail.com>
2021-12-04 18:36:34 +0100

    another line variables

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ fn main() {
     println!("Hi Mani Welcome to Learn Rust in Intellij , It is really perfect ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read Line");
-    println!("You guessed:{}",guess)
+    println!("You guessed:{}",guess);
+
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let mut name="Mani";
+    println!("The value of name is: {}", name)
+
 }
 
