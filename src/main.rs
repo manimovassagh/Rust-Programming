42f2f52fd3e0c90e5a8e14f7eb6576e248d95b3f
Mani Movassagh <mani.mgh@gmail.com>
2021-12-04 18:52:08 +0100

    another line prints

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
 
 use std::io;
 // check
-// fn main() {
-//     println!("Hi Mani Welcome to Learn Rust in Intellij , It is really perfect ");
-//     let mut guess = String::new();
-//     io::stdin().read_line(&mut guess).expect("Failed to read Line");
-//     println!("You guessed:{}",guess);
-//
-//
-//     let mut x = 5;
-//     println!("The value of x is: {}", x);
-//     x = 6;
-//     println!("The value of x is: {}", x);
-//
-//     let  name="Mani";
-//     println!("The value of name is: {}", name);
-//     println!("{}", name);
-//
-//
-//     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-// guess();
-//
-// }
-
 fn main() {
-    println!("Guess the number!");
-
-    println!("Please input your guess.");
-
+    println!("Hi Mani Welcome to Learn Rust in Intellij , It is really perfect ");
     let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read Line");
+    println!("You guessed:{}",guess);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let  name="Mani";
+    println!("The value of name is: {}", name);
+    println!("{}", name);
+
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+
 }
+//
+// fn main() {
+//     println!("Guess the number!");
+//
+//     println!("Please input your guess.");
+//
+//     let mut guess = String::new();
+//
+//     io::stdin()
+//         .read_line(&mut guess)
+//         .expect("Failed to read line");
+//
+//     println!("You guessed: {}", guess);
+// }
