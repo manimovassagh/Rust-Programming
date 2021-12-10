61b16ec7930f79db81843d16170152deda07afcf
Mani Movassagh <mani.mgh@gmail.com>
2021-12-10 16:25:11 +0100

    another line prints

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 fn main() {
     println!("Hi Mani Welcome to Learn Rust in Intellij , It is really perfect ");
     let mut guess = String::new();
+    let mut _second_guess = String::new();
     let mut guess_again = String::new();
     io::stdin().read_line(&mut guess_again).expect("Failed to read Line");
     println!("You guessed:{}", guess_again);
